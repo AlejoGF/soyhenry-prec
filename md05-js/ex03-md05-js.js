e1f171b9ab750c1c9c20b5f86403542335fc25e9
AlejoGF <agonzalezfittipaldi@gmail.com>
2024-02-02 12:12:03 -0300

    6th commit: MD05 JS exercise 03

---
+++ md05-js/ex03-md05-js.js
@@ -0,0 +1,12 @@
+// Crea una variable de tipo boolean.
+// Reemplaza el valor de null por el correspondiente.
+
+const nuevoBoolean = false;
+
+console.log(nuevoBoolean);
+
+if (typeof nuevoBoolean === "boolean") {
+    console.log("La variable es un booleano.");
+} else {
+    console.log("La variable no es un booleano.");
+}
