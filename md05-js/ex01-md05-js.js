98bbf99a5664e64439328ca4f554c16f05e06d88
AlejoGF <agonzalezfittipaldi@gmail.com>
2024-02-02 12:04:33 -0300

    4th commit: MD05 JS exercise 01

---
+++ md05-js/ex01-md05-js.js
@@ -0,0 +1,12 @@
+// Crea una variable de tipo string.
+// Reemplaza el valor de null por el correspondiente.
+
+const nuevoString = "Hola, mundo!";
+
+console.log(nuevoString);
+
+if (typeof nuevoString === "string") {
+    console.log("La variable es un string.");
+} else {
+    console.log("La variable no es un string.");
+}
