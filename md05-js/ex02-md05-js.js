f8ed96eb4f1ae15ce26fe33f275839df05c77e9f
AlejoGF <agonzalezfittipaldi@gmail.com>
2024-02-02 12:08:20 -0300

    5th commit: MD05 JS exercise 02

---
+++ md05-js/ex02-md05-js.js
@@ -0,0 +1,12 @@
+// Crea una variable de tipo number.
+// Reemplaza el valor de null por el correspondiente.
+
+const nuevoNumero = 12;
+
+console.log(nuevoNumero);
+
+if (typeof nuevoNumero === "number") {
+    console.log("La variable es un number.");
+} else {
+    console.log("La variable no es un number.");
+}
