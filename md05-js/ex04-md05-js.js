1b82dd161fc362c049ae97966d156f0e964d6dc1
AlejoGF <agonzalezfittipaldi@gmail.com>
2024-02-02 12:19:40 -0300

    7th commit: MD05 JS exercise 04

---
+++ md05-js/ex04-md05-js.js
@@ -0,0 +1,12 @@
+// Resuelve el siguiente problema matemático.
+// Reemplaza el valor de null por el correspondiente.
+
+const nuevaResta = 10 - 7 === 3;
+
+console.log(nuevaResta);
+
+if (nuevaResta) {
+    console.log("La resta es correcta.");
+} else {
+    console.log("La resta no es correcta.");
+}
