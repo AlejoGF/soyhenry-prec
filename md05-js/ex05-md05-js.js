f554eea95ca8122d2d39057874ef74a80ea7242f
AlejoGF <agonzalezfittipaldi@gmail.com>
2024-02-02 12:20:59 -0300

    8th commit: MD05 JS exercise 05

---
+++ md05-js/ex05-md05-js.js
@@ -0,0 +1,12 @@
+// Resuelve el siguiente problema matemático.
+// Reemplaza el valor de null por el correspondiente.
+
+const nuevaMultiplicacion = 10 * 4 === 40;
+
+console.log(nuevaMultiplicacion);
+
+if (nuevaMultiplicacion) {
+    console.log("La multiplicación es correcta.");
+} else {
+    console.log("La multiplicación no es correcta.");
+}
